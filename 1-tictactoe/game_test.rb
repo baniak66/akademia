2c08aa5a50ea617d3bb3f99b850a7ff168b0cc2f
Pawel Licznar <pawellicznar@gmail.com>
2017-11-26 20:35:06 +0100

    Add check_position tests and refactoring

--- 1-tictactoe/game_test.rb
+++ 1-tictactoe/game_test.rb
@@ -4,21 +4,37 @@ require 'minitest/pride'
 require_relative 'tictactoe'
 
 class TictactoeTest < Minitest::Test
+
+  def setup
+    @tictactoe = Tictactoe.new
+    @clean_board = @tictactoe.send(:new_board)
+  end
+
   def test_new_board
-    tictactoe = Tictactoe.new
-    assert_equal [" "," "," "," "," "," "," "," "," "], tictactoe.send(:new_board)
+    assert_equal [" "," "," "," "," "," "," "," "," "], @tictactoe.send(:new_board)
   end
 
   def test_user_X_win
-    tictactoe = Tictactoe.new
-    board1 = ["X","X","X","O"," ","O"," "," "," "]
-    assert tictactoe.send(:check_winer, board1, "X")
+    board = ["X","X","X","O"," ","O"," "," "," "]
+    assert @tictactoe.send(:check_winer, board, "X")
   end
 
   def test_user_X_lose
-    tictactoe = Tictactoe.new
-    board2 = ["X","O","X","O","X","O","O","X","O"]
-    refute tictactoe.send(:check_winer, board2, "X")
+    board = ["X","O","X","O","X","O","O","X","O"]
+    refute @tictactoe.send(:check_winer, board, "X")
+  end
+
+  def test_wrond_address
+    refute @tictactoe.send(:check_position, "a4", @clean_board)
+  end
+
+  def test_valid_address
+    assert @tictactoe.send(:check_position, "a1", @clean_board)
+  end
+
+  def test_duplicated_address
+    board = ["X"," "," "," "," "," "," "," "," "]
+    refute @tictactoe.send(:check_position, "a1", board)
   end
 end
 
