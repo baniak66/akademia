77cd778950b323574cc5acae27dcddf94d4053e1
Pawel Licznar <pawellicznar@gmail.com>
2018-01-11 22:58:52 +0100

    User can login & logout

--- 4-sinatra/app.rb
+++ 4-sinatra/app.rb
@@ -4,6 +4,36 @@ Bundler.require(:default)
 
 require 'sinatra/reloader'
 
+enable :sessions
+
 get '/' do
-  "HELLO"
+  if session[:logged]
+    redirect to '/login'
+  else
+    erb :form
+  end
+end
+
+post '/login' do
+  if session[:logged]
+    erb :logged
+  else
+    pass_array = File.open("password.txt").read.split(":")
+    salt = pass_array[0]
+    pass = pass_array[1]
+    timestamp = pass_array[2]
+
+    hashed_password = Digest::SHA2.new(512).hexdigest(salt+"#"+params[:password]+"#"+timestamp)
+    if hashed_password == pass
+      session[:logged] = true
+      erb :logged
+    else
+      redirect '/'
+    end
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
 end
