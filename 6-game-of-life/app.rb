bf2da7bb307395d9b595d7090d211ea3579e9546
baniak66 <pawellicznar@gmail.com>
2018-03-06 23:10:38 +0100

    Apply changes after code review

--- 6-game-of-life/app.rb
+++ 6-game-of-life/app.rb
@@ -11,7 +11,7 @@ before do
   # set size of board
   rows = 10
   cols = 10
-  @board = Array.new(rows) { Array.new(cols, "dead") }
+  @board = Array.new(rows) { Array.new(cols, 'dead') }
 end
 
 get '/' do
@@ -28,32 +28,30 @@ post '/update' do
   to_die = []
 
   params.delete('captures')
-  params.each do |field|
-    x = field.first[0,1].to_i
-    y = field.first[1,1].to_i
-    field_type = field.last
+  params.each do |key, value|
+    x = key[0].to_i
+    y = key[1].to_i
+    field_type = value
 
-    options = [[x-1, y-1], [x-1, y], [x-1, y+1], [x, y-1],
-      [x, y+1], [x+1, y-1], [x+1, y], [x+1, y+1]]
+    options = [[x - 1, y - 1], [x - 1, y], [x - 1, y + 1], [x, y - 1],
+               [x, y + 1], [x + 1, y - 1], [x + 1, y], [x + 1, y + 1]]
 
     valid_opitions = options.select do |opt|
       opt.all? { |el| el >= 0 && el < 10 }
     end
     neighbours = []
     valid_opitions.each { |opt| neighbours << params["#{opt[0]}#{opt[1]}"] }
-    positive = neighbours.count{ |el| el == "live" }
+    positive = neighbours.count { |el| el == 'live' }
 
-    if field_type == "dead" && positive == 3
+    if field_type == 'dead' && positive == 3
       to_live << "#{x}#{y}"
-    elsif field_type == "live" && (positive < 2 || positive > 3)
+    elsif field_type == 'live' && (positive < 2 || positive > 3)
       to_die << "#{x}#{y}"
     end
   end
 
-  to_live.each { |el| params[el] = "live" }
-  to_live.clear
-  to_die.each { |el| params[el] = "dead" }
-  to_die.clear
+  to_live.each { |el| params[el] = 'live' }
+  to_die.each { |el| params[el] = 'dead' }
 
   @board_state = params
   haml :board
